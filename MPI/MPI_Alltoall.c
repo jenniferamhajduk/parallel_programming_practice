3e0469747e356096e27247b98c3949596f2a27f2
jenniferamhajduk <jenniferhajduk@Jennifers-Air.localdomain>
2022-05-19 01:30:39 -0700

    fixed all to all

--- MPI/MPI_Alltoall.c
+++ MPI/MPI_Alltoall.c
@@ -1,42 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <mpi.h>
 
-
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
-    int rank, size, value;
-    int my_values[size];
-    int buffer[size];
-
     MPI_Init(&argc, &argv);
+ 
+    // Get number of processes and check that 3 processes are used
+    int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     if (size < 2 || size > 4)
     {
-        printf("Please enter a number of proccessors 2-4 processors");
+        printf("This application is meant to be run with 3 MPI processes.\n");
         MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-
     }
-
-    for (int i = 0; i < size; i++)
+  
+    // Define my value
+    int my_values[size];
+    for(int i = 0; i < 3; i++)
     {
-        my_values[i] = rand() % rank;
-        printf("rank: %d index of my_values: %d value at index: %d\n", rank, i, my_values[i]);
+        my_values[i] = rank * 300 + i * 100;
     }
-    MPI_Barrier(MPI_COMM_WORLD);
-
-
-    MPI_Alltoall(&my_values, 1, MPI_INT, buffer, 1, MPI_INT, MPI_COMM_WORLD);
-    
-    for (int i = 0; i < size; i++)
-    {
-        printf("Rank: %d Index: %d Value: %d \n", rank, i, my_values[i]);
-    }
+    printf("Process %d, my values = %d, %d, %d.\n", rank, my_values[0], my_values[1], my_values[2]);
+ 
+    int buffer_recv[size];
+    MPI_Alltoall(&my_values, 1, MPI_INT, buffer_recv, 1, MPI_INT, MPI_COMM_WORLD);
+    printf("Values collected on process %d: %d, %d, %d.\n", rank, buffer_recv[0], buffer_recv[1], buffer_recv[2]);
+ 
     MPI_Finalize();
-
+ 
     return EXIT_SUCCESS;
-
 }
